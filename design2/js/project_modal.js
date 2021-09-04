65ee43541ac54756ca9a015b0e7a08de407cb6d2
Tanmoy Sarkar <ts741127@gmail.com>
2021-09-04 16:54:11 +0530

    Design1 and 2 is ready to used [WITHOUT COMPRESS]

---
+++ design2/js/project_modal.js
@@ -0,0 +1,24 @@
+
+function selectThumbnail(project_id, thumbnail_id, least_thumbnail_id, max_thumbnail_id){
+  var featured_thumbnail = document.getElementById(`featured_${project_id.toString()}`);
+  var selected_thumbnail = document.getElementById(`thumbnail_${project_id.toString()}_${thumbnail_id.toString()}`);
+  for (var i = least_thumbnail_id; i <= max_thumbnail_id; i++) {
+    var element = document.getElementById(`thumbnail_${project_id.toString()}_${i.toString()}`);
+    element.classList.remove("active");
+  }
+  selected_thumbnail.classList.add("active");
+  featured_thumbnail.src = selected_thumbnail.src;
+}
+
+
+function openModal(id){
+  var modal = document.getElementById(`project_${id.toString()}`);
+  modal.classList.remove("hidden")
+}
+
+function closeModal(id){
+  var modal = document.getElementById(`project_${id.toString()}`);
+  modal.classList.add("hidden")
+
+}
+
