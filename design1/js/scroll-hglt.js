f7b131337f03a7083f7c912e613da92a7acb2f9e
Tanmoy Sarkar <ts741127@gmail.com>
2021-09-04 16:59:45 +0530

    design1 bug fixed

--- design1/js/scroll-hglt.js
+++ design1/js/scroll-hglt.js
@@ -1,20 +1,20 @@
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll(".main-nav-link");
-window.addEventListener("scroll", () => {
-  let current = "";
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-    if (pageYOffset >= sectionTop - sectionHeight / 3) {
-      current = section.getAttribute("id");
-    }
-  });
+// const sections = document.querySelectorAll("section");
+// const navLi = document.querySelectorAll(".main-nav-link");
+// window.addEventListener("scroll", () => {
+//   let current = "";
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     const sectionHeight = section.clientHeight;
+//     if (pageYOffset >= sectionTop - sectionHeight / 3) {
+//       current = section.getAttribute("id");
+//     }
+//   });
 
-  navLi.forEach((li) => {
-    li.classList.remove("btn-cta");
-    if (li.classList.contains(current)) {
-      li.classList.add("btn-cta");
-    }
-  });
-});
+//   navLi.forEach((li) => {
+//     li.classList.remove("btn-cta");
+//     if (li.classList.contains(current)) {
+//       li.classList.add("btn-cta");
+//     }
+//   });
+// });
 
